336cdee05de07f7c49d9ecb07958140568cd7539
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2022-12-30 13:59:38 +0900

    Modify constructor

--- src/LineSegmentDetector.cpp
+++ src/LineSegmentDetector.cpp
@@ -4,6 +4,21 @@
 
 #include "LineSegmentDetector.h"
 
+LineSegmentDetector::LineSegmentDetector(double scale, double sigma_scale, double quant, double ang_th, double log_eps,
+                                         double density_th, int n_bins) {
+    this->scale = scale;
+    this->sigma_scale = sigma_scale;
+    this->quant = quant;
+    this->ang_th = ang_th;
+    this->log_eps = log_eps;
+    this->density_th = density_th;
+    this->n_bins = n_bins;
+
+    prec = std::numbers::pi * ang_th / 180.0;
+    p = ang_th / 180.0;
+    rho = quant / std::sin(prec);
+}
+
 std::vector<Segment> LineSegmentDetector::detect(double *gray_img, int width, int height) {
     std::vector<Segment> segments;
     return segments;
--- src/LineSegmentDetector.h
+++ src/LineSegmentDetector.h
@@ -12,22 +12,33 @@
 
 class LineSegmentDetector {
 
-    double scale = 0.8;
-    double sigma_scale = 0.6;
-    double quant = 2.0;
-    double ang_th = 22.5;
-    double log_eps = 0.0;
-    double density_th = 0.7;
-    int n_bins = 1024;
-    bool grad_nfa = true;
-    double prec = std::numbers::pi * ang_th / 180.0;
-    double p = ang_th / 180.0;
-    double rho = quant / sin(prec); /* gradient magnitude threshold */
-
     public:
+        explicit LineSegmentDetector(double scale=0.8,
+                            double sigma_scal=0.6,
+                            double quant=2.0,
+                            double ang_th=22.5,
+                            double log_eps=0,
+                            double density_th=0.7,
+                            int n_bins=1024);
         std::vector<Segment> detect(double *gray_img,
                                     int width,
                                     int height);
+
+    private:
+        double scale;
+        double sigma_scale;
+        double quant;
+        double ang_th;
+        double log_eps;
+        double density_th;
+        int n_bins;
+        bool grad_nfa = true;
+        bool refine = true;
+        double prec;
+        double p;
+        double rho;
+
+        double *smoothed_img;
 
 
 };
