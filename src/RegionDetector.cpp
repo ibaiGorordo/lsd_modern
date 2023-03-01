d3e3a7354093f23579c9f5698f1d9127e6cc5656
Ibai Gorordo <ibai.gorordo@hotmail.com>
2023-03-01 21:41:30 +0900

    Revert "First part of refine completed"
    This reverts commit 822b100b699dfa57d2d388959cd941120688de3e.

--- src/RegionDetector.cpp
+++ src/RegionDetector.cpp
@@ -11,90 +11,123 @@
 #include "utils.h"
 
 RegionDetector::RegionDetector(double thres, double density_threshold) {
-    this->ang_th = thres * std::numbers::pi / 180.0;
-    this->ang_th_norm = thres / 180.0;
-    this->density_th = density_threshold;
+    this->angTh = thres * std::numbers::pi / 180.0;
+    this->angThNorm = thres / 180.0;
+    this->densityTh = density_threshold;
 }
 
 void RegionDetector::detect(const double *angles,
                             const double *magnitudes,
                             unsigned char *bad_pixels,
                             int width, int height) {
-    check_new_img_size(width, height);
+    checkNewImgSize(width, height);
 
-    angles_ptr = angles;
-    magnitudes_ptr = magnitudes;
-    used_pixels_ptr = bad_pixels;
+    anglesPtr = angles;
+    magnitudesPtr = magnitudes;
+    usedPixelsPtr = bad_pixels;
 
     // Get the sorted pixel coordinates bsaed on the magnitude that are not bad pixels
-    get_sorted_pixels();
+    getSortedPixels();
 
     // Search regions
-    search_regions();
+    searchRegions();
 
 }
 
-void RegionDetector::search_regions() {
-    region_count = 0;
+void RegionDetector::searchRegions() {
+    regionCount = 0;
 
     for(auto& point : sorted_pixels)
     {
-        if(used_pixels_ptr[point.y * img_width + point.x])
+        if(usedPixelsPtr[point.y * imgWidth + point.x])
             continue;
 
-        find_region(point.x, point.y, ang_th, min_reg_size);
+        findRegion(point.x, point.y, angTh, minRegSize);
 
-//        refine_region();
+//        refineRegion();
 
-        if(reg_density < density_th)
-            continue;
+        if(regDensity < densityTh) continue;
 
         // Rect improve
 
-        region_count++;
+
+        regionCount++;
     }
 
-    printf("Found %d regions\n", region_count);
+    printf("Found %d regions\n", regionCount);
 }
 
-void RegionDetector::refine_region() {
+void RegionDetector::refineRegion() {
 
     // Skip if already dense enough
-    if(reg_density >= density_th) return;
+    if(regDensity >= densityTh) return;
 
     // Try 1: Reduce angle tolerance
     // Recalculate the angle threshold by averaging the angle difference of the region points
     // near the seed point
-    auto new_ang_th = calculate_region_std_angle()*2;
-    auto seed_point = region_points[0];
-    find_region(seed_point.x, seed_point.y, new_ang_th, 2);
+    auto newAngTh = calculateRegionStdAngle()*2;
+    auto seedPoint = region_points[0];
+    findRegion(seedPoint.x, seedPoint.y, newAngTh, 2);
 
-    if(reg_density >= density_th) return;
+    if(regDensity >= densityTh || !regionFound) return;
 
     // Try 2: Reduce region radius
+
+    // Compute the region radius
+    auto xCenter = static_cast<double>(region_points[0].x);
+    auto yCenter = static_cast<double>(region_points[0].y);
+    auto radSq1 = point_dist_squared(xCenter, yCenter, regionRect.x1, regionRect.y1);
+    auto radSq2 = point_dist_squared(xCenter, yCenter, regionRect.x2, regionRect.y2);
+    auto radSq = radSq1 > radSq2 ? radSq1 : radSq2;
+
+    while(regDensity < densityTh)
+    {
+        // Reduce the region radius
+        radSq *= refineCoeffSq;
+
+        // Set points outside the radius to as not valid
+        for(auto& point : region_points)
+        {
+            if(point_dist_squared(xCenter, yCenter, point.x, point.y) > radSq)
+            {
+//                point.valid = false;
+                resetPoint(point);
+            }
+        }
+
+        // Check min region size 2
+        if(region_points.size() < 2) return;
+
+        calculateRect();
+    }
 }
 
-void RegionDetector::find_region(int x, int y, double angle_threrehold, int min_size) {
-    reset_region();
-    region_grow(x, y, angle_threrehold);
+void RegionDetector::calculateRect() {
+    regionRect = RegionRect(region_points, regAngle, angTh, angThNorm);
+    regDensity = getRegionDensity(regionRect, region_points.size());
+}
+
+void RegionDetector::findRegion(int x, int y, double angleThreshold, int min_size) {
+    resetRegion();
+    regionGrow(x, y, angleThreshold);
     if(region_points.size() < min_size) return;
-    region_rect = RegionRect(region_points, reg_angle, angle_threrehold, ang_th_norm);
-    reg_density = getRegionDensity(region_rect, region_points.size());
+    regionFound = true;
+    calculateRect();
 }
 
 
-void RegionDetector::get_sorted_pixels()
+void RegionDetector::getSortedPixels()
 {
     sorted_pixels.clear();
-    for(int y = 0; y < img_height; y++)
+    for(int y = 0; y < imgHeight; y++)
     {
-        auto row_start = y * img_width;
-        for(int x = 0; x < img_width; x++)
+        auto row_start = y * imgWidth;
+        for(int x = 0; x < imgWidth; x++)
         {
             int index = row_start + x;
-            if(!used_pixels_ptr[index])
+            if(!usedPixelsPtr[index])
             {
-                auto quant_norm = static_cast<uint16_t>(magnitudes_ptr[index] * quant_coeff +0.5);
+                auto quant_norm = static_cast<uint16_t>(magnitudesPtr[index] * quantCoeff +0.5);
                 if (quant_norm == 0)
                     continue;
                 sorted_pixels.emplace_back(x, y, quant_norm);
@@ -107,38 +140,38 @@ void RegionDetector::get_sorted_pixels()
 }
 
 
-void RegionDetector::region_grow(int x, int y, double angle_threrehold) {
+void RegionDetector::regionGrow(int x, int y, double angle_threrehold) {
 
 
-    register_point(x, y);
+    registerPoint(x, y);
     for(size_t i = 0; i < region_points.size(); i++) {
         auto &point = region_points[i];
 
-        int xx_min = min_limit(point.x);
-        int xx_max = max_limit(point.x, img_width);
-        int yy_min = min_limit(point.y);
-        int yy_max = max_limit(point.y, img_height);
+        int xx_min = minLimit(point.x);
+        int xx_max = maxLimit(point.x, imgWidth);
+        int yy_min = minLimit(point.y);
+        int yy_max = maxLimit(point.y, imgHeight);
 
         // Check the 8 neighbors
         for (int y_neigh = yy_min; y_neigh <= yy_max; y_neigh++) {
-            const auto row_start = y_neigh * img_width;
+            const auto row_start = y_neigh * imgWidth;
             for (int x_neigh = xx_min; x_neigh <= xx_max; x_neigh++) {
                 int index = row_start + x_neigh;
 
-                if (used_pixels_ptr[index])
+                if (usedPixelsPtr[index])
                   continue;
 
-                const auto angle = angles_ptr[index];
+                const auto angle = anglesPtr[index];
 
-                if (!is_aligned(angle, reg_angle, angle_threrehold))
+                if (!isAligned(angle, regAngle, angle_threrehold))
                     continue;
-                register_point(x_neigh, y_neigh);
+                registerPoint(x_neigh, y_neigh);
             }
         }
     }
 }
 
-double RegionDetector::calculate_region_std_angle() {
+double RegionDetector::calculateRegionStdAngle() {
     RegionPoint seedPoint = region_points[0];
     const auto seed_angle = seedPoint.angle;
     const auto seed_x = seedPoint.x;
@@ -150,8 +183,8 @@ double RegionDetector::calculate_region_std_angle() {
 
     for(auto& point : region_points)
     {
-        reset_point(point);
-        if(point_dist(point.x, point.y, seed_x, seed_y) > region_rect.width)
+        resetPoint(point);
+        if(point_dist(point.x, point.y, seed_x, seed_y) > regionRect.width)
             continue;
 
         auto angle = angle_diff_signed(point.angle, seed_angle);
@@ -167,27 +200,27 @@ double RegionDetector::calculate_region_std_angle() {
                                        count - mean_angle * mean_angle);
 }
 
-void RegionDetector::check_new_img_size(int width, int height) {
-    if (img_width == width && img_height == height) return;
+void RegionDetector::checkNewImgSize(int width, int height) {
+    if (imgWidth == width && imgHeight == height) return;
 
-    img_width = width;
-    img_height = height;
-    auto log_nt = 5 * (std::log10(double(img_width)) + std::log10(double(img_height))) / 2;
-    min_reg_size = static_cast<int>(-log_nt / std::log10(ang_th/std::numbers::pi));
+    imgWidth = width;
+    imgHeight = height;
+    auto log_nt = 5 * (std::log10(double(imgWidth)) + std::log10(double(imgHeight))) / 2;
+    minRegSize = static_cast<int>(-log_nt / std::log10(angTh /std::numbers::pi));
 }
 
-void RegionDetector::register_point(int x, int y) {
-    auto index = y * img_width + x;
-    auto angle = angles_ptr[index];
-    reg_dx += std::cos(angle);
-    reg_dy += std::sin(angle);
-    reg_angle = fast_atan2f((float) reg_dy, (float) reg_dx);
-    auto norm = magnitudes_ptr[index];
+void RegionDetector::registerPoint(int x, int y) {
+    auto index = y * imgWidth + x;
+    auto angle = anglesPtr[index];
+    regDx += std::cos(angle);
+    regDy += std::sin(angle);
+    regAngle = fast_atan2f((float) regDy, (float) regDx);
+    auto norm = magnitudesPtr[index];
     region_points.emplace_back(x, y, angle, norm);
-    used_pixels_ptr[index] = 1;
+    usedPixelsPtr[index] = 1;
 }
 
-bool RegionDetector::is_aligned(double angle, double reg_angle, double threshold) {
+bool RegionDetector::isAligned(double angle, double reg_angle, double threshold) {
 
     auto theta = reg_angle - angle;
     theta = wrap_angle(theta, pi_and_half);
@@ -196,14 +229,15 @@ bool RegionDetector::is_aligned(double angle, double reg_angle, double threshold
 }
 
 
-void RegionDetector::reset_region() {
+void RegionDetector::resetRegion() {
     region_points.clear();
-    reg_dx = 0;
-    reg_dy = 0;
-    reg_angle = 0;
-    reg_density = 0;
-    region_rect = RegionRect();
+    regionFound = false;
+    regDx = 0;
+    regDy = 0;
+    regAngle = 0;
+    regDensity = 0;
+    regionRect = RegionRect();
 }
-void RegionDetector::reset_point(RegionPoint &point) {
-    used_pixels_ptr[point.y * img_width + point.x] = 0;
+void RegionDetector::resetPoint(RegionPoint &point) {
+    usedPixelsPtr[point.y * imgWidth + point.x] = 0;
 }
