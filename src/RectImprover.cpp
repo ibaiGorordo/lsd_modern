5e7ad5d0165b9a239003b62c2f81213b92febfef
Ibai Gorordo <ibai.gorordo@hotmail.com>
2023-03-08 21:52:25 +0900

    Add nfa

--- src/RectImprover.cpp
+++ src/RectImprover.cpp
@@ -3,41 +3,104 @@
 //
 
 #include "RectImprover.h"
+#include <cmath>
 
-bool RectImprover::nfa(int totalCount, int alignedCount, double p, double logNT, double eps) {
-    if (alignedCount == 0) return false;
-    if (alignedCount == totalCount) return true;
-    double log1term = logGamma(totalCount + 1) - logGamma(alignedCount + 1) - logGamma(totalCount - alignedCount + 1);
-    double term = exp(log1term + alignedCount * log(p) + (totalCount - alignedCount) * log(1.0 - p));
-    if (term > eps) return true;
-    double binTail = term;
-    int i = alignedCount + 1;
-    while (i <= totalCount) {
-        double binCoef = log1term + log(i) - log(totalCount - i + 1);
-        binTail *= p / (1.0 - p) * (totalCount - i + 1) / i;
-        if (binCoef + log(binTail) < logNT - logEPS) return false;
-        if (binCoef + log(binTail) < logNT + logEPS) return true;
-        i++;
+constexpr double kLanczosCoefficients[] = { 75122.6331530, 80916.6278952, 36308.2951477,
+                                                  8687.24529705, 1168.92649479, 83.8676043424,
+                                                  2.50662827511 };
+
+RectImprover::RectImprover(double p, double logNt) {
+    computeNfaTable(p, logNt);
+}
+
+double RectImprover::nfa(int totalCount, int alignedCount, double p, double logNT) {
+    if (alignedCount == 0) return -logNT;
+    if (alignedCount == totalCount) return -logNT - (double) totalCount * std::log10(p);
+    auto p_term = p / (1.0-p);
+
+    auto log1term = logGamma( (double) totalCount + 1.0 ) - logGamma( (double) alignedCount + 1.0 )
+               - logGamma( (double) (totalCount-alignedCount) + 1.0 )
+               + (double) alignedCount * log(p) + (double) (totalCount-alignedCount) * log(1.0-p);
+
+    // large negative log1term, will mean term is close to 0
+    if(log1term < 10.0)
+    {
+        if( (double) alignedCount > (double) totalCount * p )     /* at begin or end of the tail?  */
+            return -log1term / M_LN10 - logNT;   /* end: use just the first term  */
+        else
+            return -logNT;
     }
-    return true;
 
+    auto term = std::exp(log1term);
+    double tolerance = 0.1;
+    auto bin_tail = term;
+    for(int i=alignedCount+1; i<=totalCount; i++)
+    {
+        auto bin_term = (double) (totalCount-i+1) * ( i<maxPopulation ?
+                                                       ( invTable[i]!=0.0 ? invTable[i] : ( invTable[i] = 1.0 / (double) i ) ) :
+                                                       1.0 / (double) i );
 
+        auto mult_term = bin_term * p_term;
+        term *= mult_term;
+        bin_tail += term;
+        if(bin_term<1.0)
+        {
+            auto err = term * ( ( 1.0 - pow( mult_term, (double) (totalCount-i+1) ) ) /
+                                  (1.0-mult_term) - 1.0 );
+
+            if( err < tolerance * std::fabs(-std::log10(bin_tail)-logNT) * bin_tail ) break;
+        }
+    }
+    return -log10(bin_tail) - logNT;
 }
 
 void RectImprover::computeNfaTable(double pInit, double logNt) {
     double p = pInit;
     int index = 0;
     for (int k = 0; k <= numImprovements; k++) {
-        for (int j = 0; j < maxPopulation; j++) {
-            for (int i = 0; i < maxPopulation; i++) {
+        for (int totalCount = 0; totalCount < maxPopulation; totalCount++) {
+            for (int goodCandidates = 0; goodCandidates <= totalCount; goodCandidates++) {
                 // TODO: Check if once the value becomes false/true, it will always be false/true
-                auto good_nfa = nfa(i, j, p, logNt, 1.0);
-                printf("Computing NFA table: %d/%d: %d\n", i,j,good_nfa);
+                auto good_nfa = nfa(goodCandidates, totalCount, p, logNt);
+                printf("Computing NFA table: %d/%d: %f\n", goodCandidates,totalCount,good_nfa);
                 //Checl when the value becomes false, store the change position and stop
 //                nfaTable[index] = nfa(i, j, p, logNt, 1.0);
-                index++;
+
             }
+            index++;
         }
         p/=2;
     }
 }
+
+constexpr double RectImprover::logGammaLanczos(double x)
+{
+    double a, b = 0.0;
+    a = (x + 0.5) * std::log(x + 5.5) - (x + 5.5);
+
+    for (int n = 0; n < 7; ++n)
+    {
+
+        a -= std::log( x + static_cast<double>(n) );
+        b += kLanczosCoefficients[n] * std::pow( x, static_cast<double>(n) );
+    }
+    return a + std::log(b);
+}
+
+constexpr double RectImprover::logGammaWindschitl(double x)
+{
+    return 0.918938533204673 + (x-0.5)*std::log(x) - x
+           + 0.5*x*std::log( x*std::sinh(1/x) + 1/(810.0*std::pow(x,6.0)) );
+}
+
+constexpr double RectImprover::logGamma(double x) {
+    if(x>15.0)
+        return logGammaWindschitl(x);
+    else
+        return logGammaLanczos(x);
+}
+
+constexpr bool RectImprover::goodNfa(double nfa, double eps) {
+    return nfa < eps;
+}
+
--- src/RectImprover.h
+++ src/RectImprover.h
@@ -19,12 +19,16 @@ private:
 
     static constexpr int maxPopulation = 1024;
     static constexpr int numImprovements = 5;
-    bool nfaTable[maxPopulation*(numImprovements+1)];
+    bool nfaTable[maxPopulation*(numImprovements+1)]{};
+    double invTable[maxPopulation]{};   /* table to keep computed inverse values */
 
 private:
     void computeNfaTable(double pInit, double logNt);
-
-    static bool nfa(int totalCount, int alignedCount, double p, double logNT, double eps);
+    double nfa(int totalCount, int alignedCount, double p, double logNT);
+    static constexpr double logGammaWindschitl(double x);
+    static constexpr double logGammaLanczos(double x);
+    static constexpr bool goodNfa(double nfa, double eps);
+    static constexpr double logGamma(double x);
 };
 
 
